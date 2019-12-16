ba30b7e087720d5cf6fc43eec6fbf104cde5b91b
Arcene LB <55070235+lbernadel@users.noreply.github.com>
2019-12-15 23:21:15 -0500

    evrything seems to be working now

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,1 +1,91 @@
-console.log("Hello from your service worker!")
+const FILES_TO_CACHE = [
+  "/",
+  "/favicon.ico",
+  "/index.html",
+  "/manifest.webmanifest",
+  "/db.js",
+  "/style.css",
+  "/index.js",
+  "icons/android-icon-36x36.png",
+  "icons/android-icon-48x48.png",
+  "icons/android-icon-72x72.png",
+  "icons/android-icon-96x96.png",
+  "icons/android-icon-144x144.png",
+  "icons/android-icon-192x192.png"
+]
+
+const CACHE_NAME = "app-cache-v1";
+
+// install
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      console.log("Your files were pre-cached successfully!");
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+
+});
+
+self.addEventListener("fetch", function (event) {
+  if (event.request.url.includes("/api/")) {
+    event.respondWith(
+      caches.open(CACHE_NAME).then(cache => {
+        return fetch(event.request)
+          .then(response => {
+            // If the response was good, clone it and store it in the cache.
+            if (response.status === 200) {
+              cache.put(event.request.url, response.clone());
+            }
+            return response;
+          })
+          .catch(err => {
+            // Network request failed, try to get it from the cache.
+            return cache.match(event.request);
+          });
+      }).catch(err => {
+        console.log(err)
+      })
+    );
+    return;
+  }
+  event.respondWith(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.match(event.request).then(response => {
+        return response || fetch(event.request);
+      });
+    })
+  );
+});
+
+// self.addEventListener("fetch", event => {
+//   if (event.request.url.includes("/api/")) {
+//     event.respondWith(
+//       caches.open(CACHE_NAME).then(cache => {
+//         return fetch(event.request)
+//           .then(response => {
+//             // If the response was good, clone it and store it in the cache.
+//             cache.put(event.request.url, response);
+
+//             return response.clone();
+//           })
+//           .catch(err => {
+//             // Network request failed, try to get it from the cache.
+//               return cache.match(event.request);
+//           });
+//       }).catch(err => {
+//         console.log(err)
+//       })
+//     );
+
+//     return;
+//   }
+
+//   event.respondWith(
+//     caches.open(CACHE_NAME).then(cache => {
+//       return cache.match(event.request).then(response => {
+//         return response || fetch(event.request);
+//       });
+//     })
+//   );
+// });
